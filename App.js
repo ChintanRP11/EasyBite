ce0118ccf0086f2c92d3c0da823e0ba4fdac9a2c
ChintanRP11 <crpansuriya.11@gmail.com>
2023-10-27 13:51:00 -0400

    elements, components and their compositions

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h2",
-  { id: "heading" },
-  "Namaste React 🚀"
-);
-console.log(heading);
-
-// JSX - HTML like syntax
-// JSX => (Babel) React.createElement => React Element - JS object => HTML element
-
-const jsxHeading = (
+// jsx element
+const title = (
   <div>
-    <h1 className="heading">Namaste React using JSX 🚀</h1>
+    <h1 className="head" tabIndex={5}>
+      Heading using JSX element
+    </h1>
   </div>
 );
-console.log(jsxHeading);
+
+// same as above but created using functional component
+const TitleComponent = () => (
+  <h1 className="head" tabIndex={5}>
+    Heading using functional component
+  </h1>
+);
+
+// react functional component
+const HeadingComponent = () => (
+  <div id="container">
+    {title}
+    {TitleComponent()}
+    <TitleComponent />
+    <TitleComponent></TitleComponent>
+    <h1 className="heading">
+      Namaste React using React Functional Component 🚀
+    </h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent />);
