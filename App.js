c608024226136dc114650f518f35dba13825a186
ChintanRP11 <crpansuriya.11@gmail.com>
2023-10-27 12:41:27 -0400

    JSX - developer friendly

--- App.js
+++ App.js
@@ -1,31 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div", 
-    {id: "parent"}, 
-    [ React.createElement(
-        "div",
-        {id: "child1"}, [ 
-            React.createElement("h1", {}, "I'm an H1 tag"), 
-            React.createElement("h2", {}, "I'm an H2 tag")
-        ]
-    ), React.createElement(
-        "div",
-        {id: "child2"}, [ 
-            React.createElement("h1", {}, "I'm an H1 tag"), 
-            React.createElement("h2", {}, "I'm an H2 tag")
-        ]
-    )]
+const heading = React.createElement(
+  "h2",
+  { id: "heading" },
+  "Namaste React 🚀"
 );
+console.log(heading);
 
-const child3 = React.createElement(
-    "div",
-    {id: "child3"}, [ 
-        React.createElement("h1", {}, "I'm an H1 tag"), 
-        React.createElement("h2", {}, "I'm an H2 tag")
-    ]
+// JSX - HTML like syntax
+// JSX => (Babel) React.createElement => React Element - JS object => HTML element
+
+const jsxHeading = (
+  <div>
+    <h1 className="heading">Namaste React using JSX 🚀</h1>
+  </div>
 );
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(heading);
